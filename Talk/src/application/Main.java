69945415a3711cdf7ebce85a824b5535bf3e45d8
kriey1 <dongjun569@naver.com>
2023-09-07 00:21:57 +0900

    첫화면 실행
    login.fxml

---
+++ Talk/src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	  Scene scene;
+	  @Override
+	    public void start(Stage stage) throws IOException {
+
+	      
+
+	        Parent root = FXMLLoader.load(getClass().getResource("/application/login.fxml"));
+
+	        Scene scene = new Scene(root);
+	       
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+
+	    public void setRoot(String fxml) throws IOException {
+	        scene.setRoot(loadFXML(fxml));
+	    }
+
+	    public Parent loadFXML(String fxml) throws IOException {
+	        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+	        return fxmlLoader.load();
+	    }
+
+	    public static void main(String[] args) {
+	        launch();
+	    }
+	
+}
---
+++ Talk/src/application/SignupController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class SignupController {
+
+    @FXML
+    void gotoMain(MouseEvent event) {
+
+    }
+
+}
