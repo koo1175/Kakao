10722c87f474e6b7ed5610b6545c92413e143c7d
kriey1 <dongjun569@naver.com>
2023-09-08 13:44:13 +0900

    채팅룸
    아래서 쓰고 위로 보내는거까지

---
+++ src/application/MainController.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+	private Singleton sgt = Singleton.getInstance(); // 싱글톤
+
+	@FXML
+	private Text goSign;
+
+	@FXML
+	private PasswordField passwordField;
+
+	@FXML
+	private TextField usernameField;
+
+	@FXML // 첫화면로그인버툰
+	void gotoMain(MouseEvent event) throws IOException {
+		
+		if(usernameField.getText().equals("a")&&passwordField.getText().equals("a")) {
+			sgt.nextScene2(event, "/application/ChatList.fxml");
+		}
+		else {
+			System.out.println("다시 입력하세요");
+		}
+		
+		
+	}
+
+	@FXML // 회원가입 화면
+	void gotoSignup(MouseEvent event) throws IOException {
+		sgt.nextScene2(event, "/application/Registration.fxml");
+	}
+
+}
---
+++ src/application/RegistrationController.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class RegistrationController {
+
+	private Singleton sgt = Singleton.getInstance(); // 싱글톤
+
+	@FXML
+    private Button Nickcheck;
+
+    @FXML
+    private TextField checkPwd;
+
+    @FXML
+    private Button gotoMain;
+
+    @FXML
+    private Button idCheck;
+
+    @FXML
+    private TextField IdInput;
+
+    @FXML
+    private TextField inputNIck;
+
+    @FXML
+    private TextField inputPwd;
+
+	@FXML
+	void gotoMain(MouseEvent event) throws IOException {
+		
+		
+		
+		sgt.nextScene(event, "/application/Login.fxml");
+		
+		
+	}
+	
+
+    @FXML
+    void checkID(MouseEvent event) {
+    	
+    	System.out.println(IdInput.getText());
+    }
+	
+	
+
+}
